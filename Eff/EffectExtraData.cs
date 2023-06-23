18b5aab17afb2b1a6377ed8843d9715c2fdd9daa
thalber <filefillman@gmail.com>
2023-06-24 00:35:54 +0400

    discard unknown fields

--- Eff/EffectExtraData.cs
+++ Eff/EffectExtraData.cs
@@ -6,10 +6,10 @@ public sealed class EffectExtraData
 	public Dictionary<string, string> RawData { get; private set; }
 	public EffectDefinition Definition { get; private set; }
 
-	private Dictionary<string, Cached<int>> _ints = new();
-	private Dictionary<string, Cached<float>> _floats = new();
-	private Dictionary<string, Cached<bool>> _bools = new();
-	private Dictionary<string, Cached<string>> _strings = new();
+	private Dictionary<string, (IntField, Cached<int>)> _ints = new();
+	private Dictionary<string, (FloatField, Cached<float>)> _floats = new();
+	private Dictionary<string, (BoolField, Cached<bool>)> _bools = new();
+	private Dictionary<string, (StringField, Cached<string>)> _strings = new();
 	public EffectExtraData(
 		RoomSettings.RoomEffect effect,
 		Dictionary<string, string> rawData,
@@ -36,31 +36,31 @@ public sealed class EffectExtraData
 			case (IntField field):
 			{
 				if (!int.TryParse(fieldstringvalue, out var result)) result = field.DefaultInt;
-				_ints[fieldname] = new(result, (newval) => rawData[fieldname] = newval.ToString());
+				_ints[fieldname] = (field, new(result, (newval) => rawData[fieldname] = newval.ToString()));
 				break;
 			}
 			case (FloatField field):
 			{
 				if (!float.TryParse(fieldstringvalue, out var result)) result = field.DefaultFloat;
-				_floats[fieldname] = new(result, (newval) => rawData[fieldname] = newval.ToString());
+				_floats[fieldname] = (field, new(result, (newval) => rawData[fieldname] = newval.ToString()));
 				break;
 			}
 			case (BoolField field):
 			{
 				if (!bool.TryParse(fieldstringvalue, out var result)) result = field.DefaultBool;
-				_bools[fieldname] = new(result, (newval) => rawData[fieldname] = newval.ToString());
+				_bools[fieldname] = (field, new(result, (newval) => rawData[fieldname] = newval.ToString()));
 				break;
 			}
 			case (StringField field):
 			{
 				//todo: escapes
-				_strings[fieldname] = new(fieldstringvalue, (newval) => rawData[fieldname] = newval ?? field.DefaultString ?? "");
+				_strings[fieldname] = (field, new(fieldstringvalue, (newval) => rawData[fieldname] = newval ?? field.DefaultString ?? ""));
 				break;
 			}
 			default:
 			{
-				plog.LogWarning($"Eff: Invalid default data setup for field {fieldname} : {fielddef}. Treating as string");
-				_strings[fieldname] = new(fieldstringvalue, (newval) => rawData[fieldname] = newval);
+				plog.LogWarning($"Eff: Invalid default data setup for field {fieldname} : {fielddef} : {fielddef.DefaultValue}. Discarding");
+				//_strings[fieldname] = (field, new(fieldstringvalue, (newval) => rawData[fieldname] = newval));
 				break;
 			}
 			}
