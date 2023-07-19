618d6ca2f452f7c776d05506e1c1aebae1e02571
thalber <filefillman@gmail.com>
2023-07-19 09:35:31 +0400

    seal field definitions

--- Eff/EffectFields.cs
+++ Eff/EffectFields.cs
@@ -15,7 +15,7 @@ public abstract record EffectField(string Name, DataType Dt, object DefaultValue
 /// <param name="Min">Minimum integer value</param>
 /// <param name="Max">Maximum integer value</param>
 /// <param name="DefaultInt">Default value</param>
-public record IntField(string Name, int Min, int Max, int DefaultInt) : EffectField(Name, DataType.Int, DefaultInt);
+public sealed record IntField(string Name, int Min, int Max, int DefaultInt) : EffectField(Name, DataType.Int, DefaultInt);
 /// <summary>
 /// A field containing a float
 /// </summary>
@@ -24,17 +24,17 @@ public record IntField(string Name, int Min, int Max, int DefaultInt) : EffectFi
 /// <param name="Max">Maximum value</param>
 /// <param name="Step">Increment for the slider</param>
 /// <param name="DefaultFloat">Default value</param>
-public record FloatField(string Name, float Min, float Max, float Step, float DefaultFloat) : EffectField(Name, DataType.Float, DefaultFloat);
+public sealed record FloatField(string Name, float Min, float Max, float Step, float DefaultFloat) : EffectField(Name, DataType.Float, DefaultFloat);
 
 /// <summary>
 /// A field containing a boolean
 /// </summary>
 /// <param name="Name">Name of the field</param>
 /// <param name="DefaultBool">Default value</param>
-public record BoolField(string Name, bool DefaultBool) : EffectField(Name, DataType.Float, DefaultBool);
+public sealed record BoolField(string Name, bool DefaultBool) : EffectField(Name, DataType.Float, DefaultBool);
 /// <summary>
 /// A field containing a string
 /// </summary>
 /// <param name="Name">Name of the field</param>
 /// <param name="DefaultString"></param>
-public record StringField(string Name, string DefaultString) : EffectField(Name, DataType.String, DefaultString ?? "");
+public sealed record StringField(string Name, string DefaultString) : EffectField(Name, DataType.String, DefaultString ?? "");
