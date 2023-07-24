ae0c4437c56f1b6d92d2a661b47678b6a8a8139f
thalber <filefillman@gmail.com>
2023-07-24 18:51:07 +0400

    example improvements

--- Eff/Examples.cs
+++ Eff/Examples.cs
@@ -24,73 +24,116 @@ internal static class Examples
 	}
 }
 
-internal class ExampleEffectUAD : UpdatableAndDeletable, IDrawable
+internal class ExampleEffectUAD : UpdatableAndDeletable
 {
-	private bool setupRan;
-	private Vector2 pos;
-	private Vector2 vel;
-	private Vector2 acceleration;
-	private Vector2 screenspace;
+	private ExampleDrawable[] _drawables;
+	private bool _setupRan;
+
 	public EffectExtraData EffectData { get; }
+
 	public ExampleEffectUAD(EffectExtraData effectData)
 	{
-		//acceleration_intensity = effectData._floats["floatfield"].valueCache;
+		_drawables = new ExampleDrawable[0];
 		EffectData = effectData;
 	}
 
 	public override void Update(bool eu)
 	{
-		if (!setupRan)
+		int drawableCount = EffectData.GetInt("intfield");
+		if (!_setupRan)
 		{
 			LogWarning($"Example effect go in room {room.abstractRoom.name}");
-			setupRan = true;
+			_setupRan = true;
 		}
-		base.Update(eu);
-		float acceleration_intensity = this.EffectData.GetFloat("floatfield");
-		acceleration = new Vector2(UnityEngine.Random.value * 2f - 1, UnityEngine.Random.value * 2f - 1) * acceleration_intensity;
-		vel += acceleration;
-		pos += vel;
-		Rect bounds = new(new(), screenspace);
-		if (!bounds.Contains(pos))
+		if (drawableCount != _drawables.Length)
 		{
-			Reset(screenspace);
+			foreach (var drawable in _drawables)
+			{
+				room.RemoveObject(drawable);
+			}
+			_drawables = new ExampleDrawable[drawableCount];
+			for (int i = 0; i < drawableCount; i++)
+			{
+				_drawables[i] = new(this);
+				room.AddObject(_drawables[i]);
+			}
 		}
 	}
 
-	public void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+	private class ExampleDrawable : UpdatableAndDeletable, IDrawable
 	{
-		sLeaser.sprites = new FSprite[1];
+		private Vector2 pos;
+		private Vector2 vel;
+		private Vector2 acceleration;
+		private Vector2 screenspace;
+		private readonly ExampleEffectUAD owner;
 
-		Reset(rCam.sSize);
-		sLeaser.sprites[0] = new FSprite("FoodCircleA")
+		public ExampleDrawable(ExampleEffectUAD owner)
 		{
-			color = Color.red,
-			scale = 2f
-		};
-		AddToContainer(sLeaser, rCam, null);
-	}
+			this.owner = owner;
+		}
+		public override void Update(bool eu)
+		{
+			base.Update(eu);
+			float acceleration_intensity = this.owner.EffectData.GetFloat("floatfield");
+			acceleration = new Vector2(UnityEngine.Random.value * 2f - 1, UnityEngine.Random.value * 2f - 1) * acceleration_intensity;
+			vel += acceleration;
+			pos += vel;
+			Rect bounds = new(new(), screenspace);
+			if (!bounds.Contains(pos))
+			{
+				Reset(screenspace);
+			}
+		}
+		private void Reset(Vector2 sSize)
+		{
+			vel = new();
+			screenspace = sSize;
+			pos = screenspace / 2f;
+			if (owner.EffectData.GetBool("boolfield"))
+			{
+				room.AddObject(new Spark(
+					((Player)room.updateList.FirstOrDefault(x => x is Player))?.firstChunk.pos ?? new(),
+					UnityEngine.Random.insideUnitCircle * 5f,
+					new(1f, 0f, 0f),
+					null,
+					15,
+					30));
+			}
+		}
 
-	private void Reset(Vector2 sSize)
-	{
-		vel = new();
-		screenspace = sSize;
-		pos = screenspace / 2f;
-	}
+		public void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+		{
+			sLeaser.sprites = new FSprite[1];
 
-	public void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
-	{
-		sLeaser.sprites[0].SetPosition(pos);
-	}
+			Reset(rCam.sSize);
+			sLeaser.sprites[0] = new FSprite("FoodCircleA")
+			{
+				color = Color.red,
+				scale = 2f
+			};
+			AddToContainer(sLeaser, rCam, null);
+		}
+		public void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+		{
+			sLeaser.sprites[0].SetPosition(pos);
+			if (!sLeaser.deleteMeNextFrame && (base.slatedForDeletetion || this.room != rCam.room))
+			{
+				sLeaser.CleanSpritesAndRemove();
+			}
+		}
 
-	public void ApplyPalette(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
-	{
+		public void ApplyPalette(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
+		{
 
-	}
+		}
 
-	public void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer? newContatiner)
-	{
-		newContatiner ??= rCam.ReturnFContainer("Foreground");
-		sLeaser.sprites[0].RemoveFromContainer();
-		newContatiner.AddChild(sLeaser.sprites[0]);
+		public void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer? newContatiner)
+		{
+			foreach (var sprite in sLeaser.sprites) sprite?.RemoveFromContainer();
+			newContatiner ??= rCam.ReturnFContainer("Foreground");
+			sLeaser.sprites[0].RemoveFromContainer();
+			newContatiner.AddChild(sLeaser.sprites[0]);
+		}
 	}
 }
