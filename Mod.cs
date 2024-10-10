6d6ea5eae07b78f7210ae01845bc0ee1804a1aa7
Bro <bro479508@gmail.com>
2024-10-09 19:41:35 -0600

    Remove Old Version
    Due to AssemblyResolve issues with MultiFolderLoader, the older version was getting loaded instead of the newer version most of the time. To sidestep this issue, since the old version just had 1 extra il hook, that il hook has been restored under the condition of having a low enough version number

--- Mod.cs
+++ Mod.cs
@@ -7,6 +7,13 @@ using MonoMod.Utils;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 
+using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
 
 
 
@@ -43,10 +50,55 @@ public class Mod : BepInEx.BaseUnityPlugin
 		{
 			LogFatal(ex);
 		}
+
+		try
+		{
+			IL.SaveUtils.PopulateUnrecognizedStringAttrs += __IL_FixUnrecognizedAttrs;
+		}
+		catch (System.Exception ex)
+		{
+			LogError($"Could not register Unrecognized fix ILHook : {ex}");
+		}
 	}
 	/// <inheritdoc/>
 	public void OnDisable()
 	{
 		Pom.Pom.DisposeStaticInputHooks();
+	}
+
+	internal void __IL_FixUnrecognizedAttrs(MonoMod.Cil.ILContext il)
+	{
+		//don't apply this hook in 1.9.14 or greater
+		string version = RainWorld.GAME_VERSION_STRING;
+		if (!version.StartsWith("v1.9.")) return; //some very different version
+		version = version["v1.9.".Length..]; //remove all but the last number
+		while (!version.IsNullOrWhiteSpace()) //don't try to parse whitespace
+		{
+			if (!int.TryParse(version, out var versionNumber)) //remove extra characters from the end, eg v1.9.15b
+			{ version = version[..^1]; }
+			else //if parsing is successful, we probably have a discernable version
+			{
+				if (versionNumber >= 14) return; //don't apply in 1.9.14 or greater
+				else break; //continue on with the method
+			}
+		}
+
+		MonoMod.Cil.ILCursor c = new(il);
+		if (c.TryGotoNext(MonoMod.Cil.MoveType.After,
+			(x) => x.MatchLdarg(1),
+			(x) => x.MatchLdarg(0),
+			(x) => x.MatchLdlen(),
+			(x) => x.MatchConvI4(),
+			(x) => x.MatchBlt(out ILLabel target)
+			))
+		{
+			c.Prev.OpCode = OpCodes.Pop;
+			c.Prev.Operand = null;
+			c.Emit(OpCodes.Pop);
+		}
+		else
+		{
+			throw new Exception("Could not find injection point; presumed an identical hook is already in place");
+		}
 	}
 }
